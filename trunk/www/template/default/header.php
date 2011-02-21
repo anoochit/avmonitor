9ba59ca3b38fc6818b57cef910d3cad7c17caccd
anoochit <anoochit@ec935c35-3ef8-1a4a-43cc-5f8b10bf5d5f>
2011-02-21 08:56:23 +0000

    update default template

--- trunk/www/template/default/header.php
+++ trunk/www/template/default/header.php
@@ -5,7 +5,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title><?=ucwords($ocreq[0]); ?> - redMonitoring : Availability Monitoring </title>
+<title><?=ucwords($ocreq[0]); ?> - avMonitoring : Availability Monitoring </title>
 <link rel="stylesheet" type="text/css" href="/template/default/style.css" />
 <link rel="Stylesheet" type="text/css" href="/include/jquery/css/ui-darkness/jquery-ui-1.8.7.custom.css"/>	
 <script type="text/javascript" src="/include/jquery/js/jquery-1.4.4.min.js"></script>
